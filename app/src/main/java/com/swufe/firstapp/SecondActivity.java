3826093190a48604e5ce0b3c891569471e13b135
liujielin123 <3304954227@qq.com>
2020-11-02 13:56:24 +0800

    计分2.0

--- app/src/main/java/com/swufe/firstapp/SecondActivity.java
+++ app/src/main/java/com/swufe/firstapp/SecondActivity.java
@@ -1,41 +1,57 @@
 package com.swufe.firstapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
-
+    TextView score;
+    TextView scoreA;
+    TextView scoreB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        score=findViewById(R.id.score);
+        scoreA=findViewById(R.id.scoreA);
+        scoreB=findViewById(R.id.scoreB);
     }
 
-
-    public void btn1(View v) {
-        show(1);
-    }
-
-    public void btn2(View v) {
-        show(2);
-    }
-
-    public void btn3(View v) {
-        show(3);
-    }
-
-    public void btnReset(View v) {
-        TextView out = (TextView)findViewById(R.id.score);
-        out.setText("0");
-    }
-
-    private void show(int i){
-        TextView out = (TextView)findViewById(R.id.score);
-        String oldScore = (String) out.getText();
-        String newScore = String.valueOf(Integer.parseInt(oldScore) + i);
-        out.setText(newScore);
-    }
-}
+    public void btnAdd1(View v) {
+        public void btnAddA1 (View v){
+            showScore(1, scoreA);
+        }
+        public void btnAdd2 (View v){
+            showScore(2);
+            public void btnAddA2 (View v){
+                showScore(2, scoreA);
+            }
+            public void btnAdd3 (View v){
+                showScore(3);
+                public void btnAddA3 (View v){
+                    showScore(3, scoreA);
+                }
+                public void btnAddB1 (View v){
+                    showScore(1, scoreB);
+                }
+                public void btnAddB2 (View v){
+                    showScore(2, scoreB);
+                }
+                public void btnAddB3 (View v){
+                    showScore(3, scoreB);
+                }
+                public void btnReset (View v){
+                    score.setText("0");
+                    scoreA.setText("0");
+                    scoreB.setText("0");
+                }
+               public void showScore ( int inc){
+                    private void showScore ( int inc, TextView score){
+                        Log.i("show", "inc=" + inc);
+                        String oldScore = (String) score.getText();
+                        score.setText("" + (Integer.valueOf(oldScore) + inc));
+                    }
+                }
